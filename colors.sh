da69e240fd9fa2b0717abfb45869d967a7c440f2
Vitaly Ivanov <ivitivan@gmail.com>
2016-05-08 21:02:54 +0300

    changes

--- colors.sh
+++ colors.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
- 
-# This program is free software. It comes without any warranty, to
-# the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What The Fuck You Want
-# To Public License, Version 2, as published by Sam Hocevar. See
-# http://sam.zoy.org/wtfpl/COPYING for more details.
- 
-#Background
-for clbg in {40..47} {100..107} 49 ; do
-	#Foreground
-	for clfg in {30..37} {90..97} 39 ; do
-		#Formatting
-		for attr in 0 1 2 4 5 7 ; do
-			#Print the result
-			echo -en "\e[${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m \e[0m"
-		done
-		echo #Newline
-	done
-done
- 
-exit 0
+#!/bin/sh
+# Print four lines showing blocks of colors: 0-7 | 0-7bold | 8-15 | 8-15bold
+perl -CADS -lwe '
+my $block = shift || (chr(0x2588) x 3);
+for (["", 0], ["1;", 0], ["", 8], ["1;", 8]) {
+  my ($bold, $offset) = @$_;
+  my @range = map $offset + $_, 0..7;
+  printf "%s %-6s ", $bold ? "bold" : "norm", "$range[0]-$range[-1]";
+  print map("\e[${bold}38;5;${_}m$block", @range), "\e[0m"
+}
+' "$@"
