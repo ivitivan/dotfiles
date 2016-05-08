060c17f0b1c338bf40df801fb39895c88ddc9fdf
Vitaly Ivanov <ivitivan@gmail.com>
2016-05-09 02:00:14 +0300

    Add install apps script

---
+++ install-apps.sh
@@ -0,0 +1,12 @@
+sudo apt-get install git
+
+git config --global user.name "Vitaly Ivanov"
+git config --global user.email ivitivan@gmail.com
+git config --global core.editor "nvim"
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+sudo apt-get install silversearcher-ag
