e45b6dc2c5fac559da87f70169fb5fc0958d36ea
Vitaly Ivanov <ivitivan@gmail.com>
2016-05-09 13:31:50 +0300

    Changes

--- install-apps.sh
+++ install-apps.sh
@@ -1,3 +1,6 @@
+#nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
 sudo apt-get install git
 
 git config --global user.name "Vitaly Ivanov"
@@ -8,5 +11,18 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.dif diff
 
 sudo apt-get install silversearcher-ag
+
+sudo apt-get install autojump
+
+if grep -q /usr/share/autojump/autojump.sh "~/.bashrc"; then
+  echo . /usr/share/autojump/autojump.sh > ~/.bashrc
+fi
+
+sudo apt-get install xsel
+
+sudo apt-get install cmake
+
+sudo apt-get install ruby
