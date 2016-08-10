d58dd160d1f30963183bdf3608687c1e0f29026f
Vitaly Ivanov <ivitivan@gmail.com>
2016-08-10 10:00:18 +0300

    Add changes

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-./config-vim.sh
-./config-tmux.sh
+git clone https://github.com/ivitivan/dotfiles /tmp/dotfiles
+cd /tmp/dotfiles
+
+cp ./configs/tmux.conf ~/.tmux.conf
+cp ./tmuxgo ~/.tmuxgo
+echo alias tm=\'bash ~/.tmuxgo\' >> ~/.bash_aliases
+
