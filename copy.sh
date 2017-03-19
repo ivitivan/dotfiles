c7052f9f91d7a63d32875cb675afce70a5be2f6a
Vitaly Ivanov <ivitivan@gmail.com>
2017-03-19 21:33:17 +0300

    Fix copy script

--- copy.sh
+++ copy.sh
@@ -1,4 +1,12 @@
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-cp -R $DIR ${HOME}
-cp -R $DIR/home-dotfiles/* ${HOME}/
+echo "copy $DIR/.config into ${HOME}"
+cp -R "$DIR/.config" "${HOME}/"
+
+shopt -s dotglob
+HOME_DOTFILES=("/home/vit/dotfiles/home-dotfiles/"*)
+for f in "${HOME_DOTFILES[@]}"
+do
+  echo "copy ${f} into ${HOME}"
+  cp -R "${f}" "${HOME}/"
+done
