de45a41143378c61f966021d7b4254a48504bd3e
Vitaly Ivanov <vitaliy.ivanov@restream.rt.ru>
2018-07-02 12:07:07 +0300

    Add git-configure script

---
+++ configure-git.sh
@@ -0,0 +1,14 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.dif diff
+git config --global credential.helper cache
+git config --global core.editor nvim
+git config --global branch.autoSetupMerge always
+
+git config --global user.email 'ivitivan@gmail.com'
+git config --global user.name 'Vitaly Ivanov'
+git config --global core.excludesfile ~/.gitignore_global
+
+git config --global push.default current
--- install.sh
+++ install.sh
@@ -45,19 +45,6 @@ gsettings set org.gnome.desktop.background show-desktop-icons false
 
 mkdir -p ~/.config/gnome-terminal-themes
 
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
-git config --global alias.dif diff
-git config --global credential.helper cache
-git config --global core.editor nvim
-git config --global branch.autoSetupMerge always
-
-git config --global user.email 'ivitivan@gmail.com'
-git config --global user.name 'Vitaly Ivanov'
-git config --global core.excludesfile ~/.gitignore_global
-
 mkdir ~/.bash
 git clone git://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
 
